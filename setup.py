0d7356e27e39df521911bf23e453f5cceb0887ea
caglorithm <caglorithm@gmail.com>
2020-07-03 15:14:18 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,13 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open("requirements.txt") as f:
-    requirements = f.read().splitlines()
+# with open("requirements.txt") as f:
+#     requirements = f.read().splitlines()
 
 setuptools.setup(
     name="tgbot",
     version="0.0.1",
-    description="Simple telegram bot",
+    description="A super simple Telegram bot.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/caglroithm/tgbot",
@@ -25,6 +25,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
-    install_requires=requirements,
+    # install_requires=requirements,
     include_package_data=True,
 )
